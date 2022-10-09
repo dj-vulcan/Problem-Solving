0b25e6c77e0ae9eeea6994d9b1fc265f6f4a5ece
didi.j <didi.j@kakaocorp.com>
2022-10-09 17:03:39 +0900

    다익스트라 개선

--- src/main/java/com/dj/algorithm/dijkstra/Dijkstra.java
+++ src/main/java/com/dj/algorithm/dijkstra/Dijkstra.java
@@ -1,15 +1,20 @@
 package com.dj.algorithm.dijkstra;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public class Dijkstra {
+
     public static void main(String[] args) {
         int[][] graph = {
             {0, 0, 6, 3, 0},
-            {3, 0, 0, 0, 0},
-            {0, 0, 0, 2, 0},
-            {0, 1, 1, 0, 0},
-            {0, 4, 0, 2, 0},
+            {0, 0, 0, 1, 2},
+            {6, 0, 0, 1, 0},
+            {3, 1, 1, 0, 1},
+            {0, 2, 0, 1, 0},
         };
         int[] result = new Dijkstra().dijkstra(graph, 4);
         Arrays.stream(result).forEach(System.out::println);
@@ -17,41 +22,33 @@ public class Dijkstra {
 
     public int[] dijkstra(int[][] graph, int s) {
         int n = graph.length;
-        boolean[] visited = new boolean[n];
+
         int[] result = new int[n];
         Arrays.fill(result, Integer.MAX_VALUE);
-
         result[s] = 0;
-        while(!isAllTrue(visited)) {
+
+        Set<Integer> visited = new HashSet<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(n, Comparator.comparingInt(a -> result[a]));
+        pq.add(s);
+        while(visited.size() < n) {
             //후보중 제일 거리짧은거 고르기
-            int minDist = Integer.MAX_VALUE;
-            int minIdx = s;
-            for(int i = 0; i < n; i++) {
-                if(!visited[i] && minDist > result[i]) {
-                    minIdx = i;
-                    minDist = result[i];
-                }
-            }
+            int minIdx = pq.remove();
 
             //neighbor 갱신
-            visited[minIdx] = true;
+            visited.add(minIdx);
             for(int i = 0; i < n; i++){
                 if(graph[minIdx][i] <= 0) {
                     continue;
-                } 
-                result[i] = Math.min(result[i], result[minIdx] + graph[minIdx][i]);
+                }
+
+                int newDist = result[minIdx] + graph[minIdx][i];
+                if(newDist < result[i]) {
+                    result[i] = newDist;
+                    pq.add(i);
+                }
             }
         }
 
         return result;
-    }
-
-    public boolean isAllTrue(boolean[] arr) {
-        for(boolean b: arr) {
-            if(!b) {
-                return false;
-            }
-        }
-        return true;
     }
 }
